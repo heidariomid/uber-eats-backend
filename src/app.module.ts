0e1cdaa99e7cc3893b66d25e48c00df3e19e0029
omidheidari88 <omid_heidari88@yahoo.com>
2022-08-06 21:42:46 +0430

    v1.0.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -69,6 +69,7 @@ import { UploadsModule } from './uploads/uploads.module';
       database: process.env.DB_DATABASE,
       schema: process.env.DB_SCEMA,
       logging: false,
+      ssl: true,
       synchronize: process.env.NODE_ENV !== 'production',
       entities: [
         Restaurant,
